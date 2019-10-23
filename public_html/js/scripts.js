6425382e7ae4497771d29f29cca79f5a23f2348a
maxivity <soroosh081@gmail.com>
2019-10-23 22:57:01 +0330

    Final Template

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -5,3 +5,23 @@ $(document).ready(function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
     });
 });
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+    relativeInput: true
+});
+var scene1 = document.getElementById('scene1');
+var parallaxInstance1 = new Parallax(scene1, {
+    relativeInput: true
+});
+var scene2 = document.getElementById('scene2');
+var parallaxInstance2 = new Parallax(scene2, {
+    relativeInput: true
+});
+var scene3 = document.getElementById('scene3');
+var parallaxInstance3 = new Parallax(scene3, {
+    relativeInput: true
+});
+var scene4 = document.getElementById('scene4');
+var parallaxInstance4 = new Parallax(scene4, {
+    relativeInput: true
+});
